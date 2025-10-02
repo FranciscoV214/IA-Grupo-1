14e9b4d8394ca2928641c308d9400724e6345a3f
FranciscoV214 <axel.francisco.villarreal@gmail.com>
2025-10-02 10:51:46 -0300

    Creo que ejercicio 4 terminado

--- TP N8/Ejercicio4.py
+++ TP N8/Ejercicio4.py
@@ -18,31 +18,33 @@ Q = np.zeros((n_states, n_states))
 
 # Parámetros
 gamma = 0.9   # factor de descuento
-alpha = 0.8   # tasa de aprendizaje
-epochs = 1000
+alpha = 0.1   # tasa de aprendizaje
+epochs = 10000
 
 # Q-Learning
 for _ in range(epochs):
     # elegir estado inicial aleatorio
-    state = np.random.randint(0, n_states) # estado inicial aleatorio
+    state = np.random.randint(0, n_states) # estado inicial aleatorio, me posiciono en un elemento de la matriz aleatorio.
     
     # encontrar acciones posibles
-    possible_actions = np.where(R[state] >= 0)[0] #desde el estado inicial, se fija a donde puede ir, elige números mayores o iguales a 0 (el único es 100 jajaj)
+    possible_actions = np.where(R[state] >= 0)[0] #desde el estado inicial, se fija a donde puede ir, elige números mayores o iguales a 0 (pueden ser 100 o 0)
     
-    # elegir una acción aleatoria
-    action = np.random.choice(possible_actions)
+    action = np.random.choice(possible_actions) #elige alguno de los caminos a los que puede ir aleatoriamente.
     
-    # calcular siguiente estado
-    next_state = action
+    next_state = action #el camino elegido es el próximo estado.
     
     # actualizar Q
-    Q[state, action] = Q[state, action] + R[state, action] + gamma * (np.max(Q[next_state]) - Q[state, action])
+    Q[state, action] = Q[state, action] + alpha * (R[state, action] + gamma * (np.max(Q[next_state]) - Q[state, action]))
 
 # Configuración para que NumPy muestre todas las filas y columnas sin cortar
 np.set_printoptions(suppress=True, linewidth=150, precision=2)
+print("Matriz de Recompensas R:")
+print(R)
+print("\n")
 
 print("Matriz Q entrenada:")
 print(Q)
+print("\n")
 
 # Normalización para interpretar mejor
 print("\nMatriz Q normalizada:")
